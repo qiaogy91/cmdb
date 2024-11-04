35585096a977e59f89e587c2e2f5437f04e681b8
qiaogy91 <9078759+qiaogy91_admin@user.noreply.gitee.com>
2024-11-04 10:12:46 +0800

    去掉Otlp 进行测试

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	_ "github.com/qiaogy91/cmdb/apps"
-	_ "github.com/qiaogy91/ioc/config/otlp"
 	"github.com/qiaogy91/ioc/server"
 	_ "github.com/qiaogy91/maudit/provider/producer"
 	_ "github.com/qiaogy91/mcenter/provider/middleware"
