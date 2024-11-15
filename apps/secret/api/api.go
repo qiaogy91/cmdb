b68290d19ed7d082efc359d5dbb3e640a61e31e4
qiaogy91 <9078759+qiaogy91_admin@user.noreply.gitee.com>
2024-11-15 19:06:17 +0800

    优化同步请求构造方式

--- apps/secret/api/api.go
+++ apps/secret/api/api.go
@@ -1,12 +1,14 @@
 package api
 
 import (
-	restfulspec "github.com/emicklei/go-restful-openapi/v2"
+	"github.com/emicklei/go-restful/v3"
 	"github.com/gorilla/websocket"
 	"github.com/qiaogy91/cmdb/apps/secret"
 	"github.com/qiaogy91/ioc"
 	"github.com/qiaogy91/ioc/config/gorestful"
 	"github.com/qiaogy91/ioc/config/log"
+	"github.com/qiaogy91/ioc/labels"
+	"github.com/qiaogy91/ioc/utils"
 	"log/slog"
 	"net/http"
 )
@@ -34,32 +36,46 @@ func (h *Handler) Init() {
 	h.Registry()
 }
 
+type User struct {
+	Name string
+	Age  int
+}
+
+type UserSet struct {
+	total int
+	items []*User
+}
+
 func (h *Handler) Registry() {
 	tag := []string{"凭证管理"}
 
 	ws := gorestful.ModuleWebservice(h)
-	ws.Route(ws.GET("/").To(h.Test).
-		Doc("测试接口").
-		Metadata(restfulspec.KeyOpenAPITags, tag).
-		Metadata("auth", true).
-		Metadata("audit", true).
-		Metadata("perm", true).
-		Metadata("resource", secret.AppName).
-		Metadata("action", "get"),
-	)
-
 	ws.Route(ws.GET("/SyncResource").To(h.SyncResource).
 		Param(ws.QueryParameter("vendor", "厂商类型").DataType("string")).
 		Param(ws.QueryParameter("secretId", "秘钥ID")).
 		Param(ws.QueryParameter("accessEndpoint", "服务地址")).
 		Param(ws.QueryParameter("regionId", "区域ID")).
 		Doc("数据同步").
-		Metadata(restfulspec.KeyOpenAPITags, tag).
-		Metadata("auth", false).
-		Metadata("audit", true).
-		Metadata("perm", false).
-		Metadata("resource", secret.AppName).
-		Metadata("action", "sync"),
+		Metadata(labels.ApiTags, tag).
+		Metadata(labels.AuthEnabled, false).
+		Metadata(labels.AuditEnabled, true).
+		Metadata(labels.PermissionEnabled, false).
+		Metadata(labels.Resource, secret.AppName).
+		Metadata(labels.Action, "sync").
+		Metadata(labels.Desc, "资源同步"),
+	)
+
+	ws.Route(ws.GET("test").To(func(r *restful.Request, w *restful.Response) {
+		utils.SendSuccess(w, "ok")
+	}).
+		Doc("测试一下子").
+		Metadata(labels.ApiTags, tag).
+		Metadata(labels.AuthEnabled, true).
+		Metadata(labels.AuditEnabled, true).
+		Metadata(labels.PermissionEnabled, true).
+		Metadata(labels.Resource, secret.AppName).
+		Metadata(labels.Action, "get").
+		Metadata(labels.Desc, "测试资源"),
 	)
 }
 
--- apps/secret/api/secret.go
+++ apps/secret/api/secret.go
@@ -4,34 +4,14 @@ import (
 	"github.com/emicklei/go-restful/v3"
 	"github.com/qiaogy91/cmdb/apps/secret"
 	"github.com/qiaogy91/ioc/utils"
-	"log/slog"
-	"strconv"
 )
 
-func (h *Handler) Test(r *restful.Request, w *restful.Response) {
-	utils.SendSuccess(w, "ok")
-	h.log.Info("test")
-}
-
 func (h *Handler) SyncResource(r *restful.Request, w *restful.Response) {
-	// 获取请求参数 sync request
-	vid, err := strconv.ParseInt(r.QueryParameter("vendor"), 10, 32)
-	if err != nil {
-		utils.SendFailed(w, ErrSecretParams(err))
-		return
-	}
-	sid, err := strconv.ParseInt(r.QueryParameter("secretId"), 10, 64)
-	if err != nil {
-		utils.SendFailed(w, ErrSecretParams(err))
-		return
-	}
-
 	// 构建请求
-	req := &secret.SyncResourceRequest{
-		Vendor:         secret.Vendor(vid),
-		SecretId:       sid,
-		AccessEndpoint: r.QueryParameter("accessEndpoint"),
-		RegionId:       r.QueryParameter("regionId"),
+	req := &secret.SyncResourceRequest{}
+	if err := utils.Decoder.Decode(req, r.Request.URL.Query()); err != nil {
+		utils.SendFailed(w, ErrSecretParams(err))
+		return
 	}
 
 	// 升级协议
@@ -49,7 +29,5 @@ func (h *Handler) SyncResource(r *restful.Request, w *restful.Response) {
 	}
 
 	// 结束标识
-	if err := conn.WriteJSON("资源同步结束"); err != nil {
-		h.log.Error("send ws response err", slog.Any("err", err))
-	}
+	utils.WebsocketSendSuccess(conn, "资源同步结束")
 }
